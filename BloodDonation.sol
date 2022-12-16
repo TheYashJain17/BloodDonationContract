324483b3a5e8ca875fa5ee903f784994604a1491
yash jain <yashjain07.yj@gmail.com>
2022-12-16 14:03:13 +0530

    Made A registerPatient Function To Register The Patient

--- BloodDonation.sol
+++ BloodDonation.sol
@@ -9,20 +9,39 @@ contract bloodDonation{
         string Name;
         uint Age;
         string BloodGroup;
-        uint contact;
+        uint Contact;
         string HomeAddress;
-        uint aadhar;
+        uint Aadhar;
 
 }
 
-Patient[] patientDetais;
+Patient[] patientDetails;
 
-mapping(uint => uint) patientDetaisIndex;
+mapping(uint => uint) patientDetailsIndex;
 
 
 
 function getAllRecords() view external returns(Patient[] memory){
-    return patientDetais;
+    return patientDetails;
+}
+
+
+function registerPatient(string memory _name , uint _age , string memory _BloodGroup , uint _contact , string memory _HomeAddress , uint _Aadhar) external {
+
+    uint index = patientDetails.length;
+
+    patientDetails.push();
+
+    patientDetais[index].Name = _name; 
+    patientDetais[index].Age = _age;
+    patientDetais[index].BloodGroup = _BloodGroup;
+    patientDetais[index].contact = _contact;
+    patientDetais[index].HomeAddress = _HomeAddress;
+    patientDetais[index].Aadhar = _Aadhar;
+  
+    patientDetailsIndex[_Aadhar] = index;
+
+
 }
 
 
