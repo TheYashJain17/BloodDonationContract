b7f7d48f3f2624ba80c4de776c0979f56de9698d
yash jain <yashjain07.yj@gmail.com>
2022-12-19 19:19:02 +0530

    Added Comments

--- BloodDonation.sol
+++ BloodDonation.sol
@@ -3,12 +3,16 @@ pragma solidity ^0.8.9;
 
 contract bloodDonation{
 
+//Declaring An Enum With Name patientType To Know Whether The Patient Is Donor Or Receiver.
+
         enum patientType{
 
             donor,
             receiver
 
         }
+
+//Declaring Struct With Name bloodTransaction To Record The Details Of bloodTransaction.
 
         struct bloodTransaction{
 
@@ -19,6 +23,8 @@ contract bloodDonation{
 
         }
 
+//Declaring Struct With Name Patient To Record The Details Of A Patient.
+
     struct Patient{
 
         string Name;
@@ -27,49 +33,58 @@ contract bloodDonation{
         uint Contact;
         string HomeAddress;
         uint Aadhar;
-        bloodTransaction[] bT;
+        bloodTransaction[] bT;  //Making An Array With Name bT Through bloodtransaction Struct To Store The Details In Pateint Struct Which We Stored Inside The bloodTransaction Struct. 
 
 }
 
-Patient[] patientDetails;
+Patient[] patientDetails;   //Making An Array With Patient Struct To Store All Details Inside An Array. 
 
-mapping(uint => uint) patientDetailsIndex;
+mapping(uint => uint) patientDetailsIndex;  //Declaring Mapping To Access Patient Location With The Help Of His/Her Aadhar.
 
-
+//Making A Function Through Which Hospital Can Get All Records At One Go.
 
 function getAllRecords() view external returns(Patient[] memory){
     return patientDetails;
 }
 
+//Making A Function Through Which Hospital Can Get Record Of A Particular Patient.
+
 function getParticularRecord(uint _Aadhar) view external returns(Patient memory){
     
-    uint index = patientDetailsIndex[_Aadhar];
+    uint index = patientDetailsIndex[_Aadhar];  //Accessing The Patient Location With The Help Of His/Her Aadhar.
 
-    return patientDetails[index];
+    return patientDetails[index];   //Getting The Data Of That Particular Patient From Array.
 
 }
 
+//Making A Function Through Which Patients Can Register Themselves.
 
-function registerPatient(string memory _name , uint _age , string memory _BloodGroup , uint _contact , string memory _HomeAddress , uint _Aadhar) external {
+function registerPatient(string memory _Name , uint _Age , string memory _BloodGroup , uint _Contact , string memory _HomeAddress , uint _Aadhar) external {
 
-    uint index = patientDetails.length;
+    uint index = patientDetails.length; //Storing The Array's Length Into A Variable Which Is 0 For Now.
 
-    patientDetails.push();
+    patientDetails.push();  //Sending An Empty Push Into The Array Because We Cannot Enter Something Directly On The Index If It Doesnt Have Something. 
 
-    patientDetails[index].Name = _name; 
-    patientDetails[index].Age = _age;
+//Using Dot Method To Enter Data Into Our Array.Using Index Variable Here Because We Want To Start Our Data's Index From 0 And Because Of Empty Push Array's Length Has Become 1,Therefore We Stored The Value Of Array's Length Into A Variable So That We Can Store Data At 0th Index. 
+
+    patientDetails[index].Name = _Name; 
+    patientDetails[index].Age = _Age;
     patientDetails[index].BloodGroup = _BloodGroup;
-    patientDetails[index].Contact = _contact;
+    patientDetails[index].Contact = _Contact;
     patientDetails[index].HomeAddress = _HomeAddress;
     patientDetails[index].Aadhar = _Aadhar;
   
-    patientDetailsIndex[_Aadhar] = index;
+    patientDetailsIndex[_Aadhar] = index;   //Storing The Above Data(Which We Stored In Index Variable) In Refrence With The Aadhar With The Help Of Mapping.This Means This Particular Aadhar Contains This Particular Person/Patient's Information
 
 }
 
+//Making A Function Through Which Hospital Can Add The Details Of A BloodTrasaction Into The Particular Patient Details. 
+
 function Bloodtransaction(patientType _type , address _from , address _to , uint _Aadhar) external {
 
-    uint index = patientDetailsIndex[_Aadhar];
+    uint index = patientDetailsIndex[_Aadhar];  //Accessing The Patient Location With The Help Of His/Her Aadhar.
+
+//Entering The Data Of bloodTransaction Struct Into Particular Patient's Struct.
 
     patientDetails[index].bT.push(bloodTransaction(_type , block.timestamp , _from , _to));
 
